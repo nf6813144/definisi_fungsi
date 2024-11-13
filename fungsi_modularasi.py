187016c68527a13baa5d1be23a295318b61ca1d6
nf6813144 <nf6813144@gmail.com>
2024-11-13 14:10:11 +0800

    Create fungsi_modularasi.py

---
+++ fungsi_modularasi.py
@@ -0,0 +1,16 @@
+#fungsi dan mudularasi
+#fungsi mencari luas persegi panjang
+def luaspersegipanjang (panjang, lebar):
+    return panjang*lebar
+
+#funngsi mencari keliling persegi panjang
+def kelilingpersegipanjang(panjang,lebar):
+    return 2*(panjang+lebar)
+
+t=float(input("masukkan panjang :"))
+i=float(input("masukkan lebar :"))
+
+hasil_luas = luaspersegipanjang(t,i)
+hasil_keliling = kelilingpersegipanjang(t,i)
+print("luasnya adalah ", hasil_luas)
+print("keliling adalah ", hasil_keliling)
